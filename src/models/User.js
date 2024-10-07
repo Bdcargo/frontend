f634d5120bdbbf78ee3ad8a9f03b5dd12f691ca7
David Adebayo <davidadebayo702@gmail.com>
2024-10-07 04:42:06 +0100

    complete

--- src/models/User.js
+++ src/models/User.js
@@ -12,6 +12,11 @@ const UserSchema = new mongoose.Schema({
     number_referred: { type: Number, default: 0 }, // Number of users referred
     transaction_hash: { type: String }, // Transaction hash
   },
+  referredBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Who referred this user
+  referrals: [{ 
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Users referred by this user
+    status: { type: String, enum: ['inactive', 'active'], default: 'inactive' }
+  }]
 });
 
 export default mongoose.models.User || mongoose.model('User', UserSchema);
